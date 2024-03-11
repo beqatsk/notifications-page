6a10efc508531ce224bfaea9701416f51e835f66
beqa <beqa.tskhvediani@gmail.com>
2024-03-11 10:30:07 +0400

    add GlobalStyles

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import "./reset.css";
+import styled from "styled-components";
+function App() {
+  return (
+    <>
+      <Container>hi</Container>
+    </>
+  );
+}
+const Container = styled.div`
+  width: 375px;
+  padding: 24px 16px 29px;
+  background-color: #fff;
+  border: 1px solid red;
+`;
+
+export default App;
---
+++ src/GlobalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+export const GlobalStyles = createGlobalStyle`
+body{
+    background-color: white;
+    font-family: "Roboto", sans-serif;
+    max-width: 100%;
+    min-height: 100vh;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+`;
