c5d5a62dfbfee5656ddfd94937ce4ed6d25762f9
beqa <beqa.tskhvediani@gmail.com>
2024-03-15 14:00:20 +0400

    add useState

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,40 @@
 import styled from "styled-components";
 import date from "../data.json";
+import { useState } from "react";
 
 export default function Main() {
+  const [notifications, setNotifications] = useState(date);
+
   return (
     <NotificationWrapper>
-      {date.map((item, index) => (
-        <Notification key={index}>
-          <img src={item.url} alt="avatar" />
-          <ParagraphWrapper>
-            <Paragraph>
-              <SpanName>{item.name}</SpanName> {item.title}{" "}
-              <SpanInfo>{item.event}</SpanInfo>
-              <Oval></Oval>
-            </Paragraph>
-            <SpanTime>{item.date}</SpanTime>
-          </ParagraphWrapper>
-        </Notification>
-      ))}
+      {notifications.map((item) => {
+        return (
+          <Notification key={item.id}>
+            <img src={item.url} alt="avatar" />
+            <ParagraphWrapper>
+              <Wrapper>
+                <div>
+                  <Paragraph>
+                    <SpanName>{item.name}</SpanName> {item.title}{" "}
+                    <SpanInfo>{item.event}</SpanInfo>
+                    <Oval></Oval>
+                  </Paragraph>
+                  <SpanTime>{item.date}</SpanTime>
+                </div>
+
+                {item.userImage ? (
+                  <div>
+                    <img src={item.userImage} alt="" />
+                  </div>
+                ) : null}
+              </Wrapper>
+              {item.message ? (
+                <MessageBox index={item.id}>{item.message}</MessageBox>
+              ) : null}
+            </ParagraphWrapper>
+          </Notification>
+        );
+      })}
     </NotificationWrapper>
   );
 }
@@ -30,10 +48,11 @@ const Notification = styled.div`
   width: 343px;
   display: flex;
   gap: 13px;
-  align-items: center;
+  align-items: start;
   border-radius: 8px;
   background-color: #f7fafd;
   padding: 16px 26px 16px 16px;
+  cursor: pointer;
 `;
 const Paragraph = styled.p`
   font-size: 14px;
@@ -76,6 +95,20 @@ const SpanTime = styled.span`
 `;
 const ParagraphWrapper = styled.div`
   display: flex;
+
   flex-direction: column;
   gap: 4px;
+`;
+const MessageBox = styled.div`
+  width: 263px;
+  padding: ${(props) => (props.index == 4 ? "24px 16px 29px" : "")};
+  border: ${(props) => (props.index == 4 ? "1px solid #dde7ee" : "")};
+  width: 231px;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: normal;
+  color: #5e6778;
+`;
+const Wrapper = styled.div`
+  display: flex;
 `;
